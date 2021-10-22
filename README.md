c91feb0c5cbbbed4bc67c31af765d05296196eb6
Ronald Tadeu Alves Pinto Filho <50664977+julado123br@users.noreply.github.com>
2021-10-22 20:32:26 -0300

    chat arr

--- README.md
+++
@@ -1,2 +0,0 @@
-# CHATPHP
- chat feito em php puro
---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '573f82efc30dee594d7ad69b359755d0a1a6f902',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '573f82efc30dee594d7ad69b359755d0a1a6f902',
+            'dev_requirement' => false,
+        ),
+        'psr/log' => array(
+            'pretty_version' => '1.1.4',
+            'version' => '1.1.4.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../psr/log',
+            'aliases' => array(),
+            'reference' => 'd49695b909c3b7628b6289db5479a1c204601f11',
+            'dev_requirement' => false,
+        ),
+        'textalk/websocket' => array(
+            'pretty_version' => '1.5.5',
+            'version' => '1.5.5.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../textalk/websocket',
+            'aliases' => array(),
+            'reference' => '846542f82658132cd36acb7a7e8ce0f03960c295',
+            'dev_requirement' => false,
+        ),
+    ),
+);
